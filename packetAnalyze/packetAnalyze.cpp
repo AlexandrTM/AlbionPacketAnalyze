57ea6b18ff4002cd74405210a82a77d60ee212b5
Alexandr K <sashakddr16@gmail.com>
2023-02-20 15:33:06 +0300

    1.3 splitting packet into commands

--- packetAnalyze/packetAnalyze.cpp
+++ packetAnalyze/packetAnalyze.cpp
@@ -29,14 +29,25 @@ void printPacket(RawPDU pdu, size_t regionStart, size_t regionEnd) {
 }
 void readPacket(RawPDU pdu, size_t regionStart, size_t regionEnd, std::stringstream &ss)
 {
-    for (size_t i = 16; i < 20; i++)
-        //for (size_t i = 0; i < pdu.rfind_pdu<RawPDU>().payload_size(); i++)
+    for (size_t i = regionStart; i < regionEnd; i++)
     {
         uint8_t x = pdu.payload()[i];
         if (x < 16)
             ss << 0;
         ss << std::hex << (int)x;
     }
+}
+std::stringstream readPacket(RawPDU pdu, size_t regionStart, size_t regionEnd)
+{
+    std::stringstream ss;
+    for (size_t i = regionStart; i < regionEnd; i++)
+    {
+        uint8_t x = pdu.payload()[i];
+        if (x < 16)
+            ss << 0;
+        ss << std::hex << (int)x;
+    }
+    return ss;
 }
 
 bool callback(const PDU& pdu) {
@@ -46,17 +57,49 @@ bool callback(const PDU& pdu) {
     RawPDU rawPDU = pdu.rfind_pdu<RawPDU>();
     if (albionIPRange.contains(ip.src_addr()) and udp.sport() == 5056)
     { 
-        std::stringstream ss;
         if (rawPDU.payload_size() > 23)
         {
-            readPacket(rawPDU, 16, 20, ss);
+            // 0-4 num of commnads in packet, 
+            // 4-8 time, not 20-22, eventcandidate 36-40
+            // 8-12 location id randomly generates each time
+            // 12-16 only 5 types exists 
+            // 07000000 06000100 01000000 01ff0000 08000100
+            // 16-20 event code ?
+            // one byte is 2
+            std::vector<std::string> compareStrings = 
+            {"01000000", "01ff0000", "06000100", "07000000", "08000100"};
+            std::stringstream ss;
+            std::stringstream compareString;
+            std::vector<size_t> eventParts;
 
-            if (ss.str() != "0000001c")
+            if (ss.str() != "0fff000")
             {
-                printPacket(rawPDU, 12, 16);
+                size_t counter = 0;
+                for (size_t i = 12; i < (rawPDU.payload_size() - 4); i++)
+                { 
+                    readPacket(rawPDU, i, i + 4, compareString);
+                    for (size_t j = 0; j < compareStrings.size(); j++)
+                    {
+                        if (compareString.str() == compareStrings[j])
+                        {
+                            eventParts.push_back(i);
+                            i += 7;
+                        }
+                    }
+                    compareString.str(std::string());
+                }
+                eventParts.push_back(rawPDU.payload_size());
+                std::cout << "event parts: \n";
+                for (size_t i = 0; i < eventParts.size() - 1; i++)
+                {
+                    printPacket(rawPDU, eventParts[i], eventParts[i + 1]);
+                    std::cout << "\n";
+                }
+                //std::cout << "counter: " << counter << "\n";
+                /*std::cout << "packet: \n";
+                printPacket(rawPDU);
+                std::cout << "\n";*/
                 //std::cout << stoi(ss.str());
-                //std::cout << std::dec << "\n" << "Packet size: " 
-                //    << rawPDU.payload_size() << "\n";
             }
             
         }
